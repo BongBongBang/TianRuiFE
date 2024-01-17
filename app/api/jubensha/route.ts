d9e57596545c00a1e4fd163ecc8f40527b7e276b
PC/Aaron <343129625@qq.com>
2024-01-17 22:49:43 +0800

    preview

---
+++ app/api/jubensha/route.ts
@@ -0,0 +1,46 @@
+import { NextResponse } from "next/server"
+import OpenAI from 'openai'
+import { kv } from '@vercel/kv'
+import { OpenAIStream, StreamingTextResponse } from 'ai'
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+  })
+  
+export const runtime = 'edge'
+
+export async function POST(req: Request) {
+    const json = await req.json()
+    const { idGt, idLt } = json
+    const result = (await fetch(`http://47.104.74.127:8080/fetch_comment?idGt=${idGt}&idLt=${idLt}`, {method: 'GET'}));
+    const array = await result.json();
+
+    for (let i = 0; i < array.length; i++) {
+
+        const item = array[i]
+        console.log('Start ', item.ScriptEvaluateId); 
+        const messages : any = [{role:'user', content: item.EvaluateTextContent}];
+        const res = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo',
+            messages,
+            temperature: 0.7,
+            stream: false
+        })
+
+        const convertedContent = res['choices'][0]['message']['content']
+        const body : any = [{
+            "scriptEvaluateId": item.ScriptEvaluateId,
+            "scriptId": item.ScriptId,
+            "evaluateTextContent": convertedContent
+        }]
+        const result = await fetch(`http://47.104.74.127:8080/save_converts`, {
+            method: 'POST',
+            body: JSON.stringify(body)
+        });
+        if (result.ok) {
+            await kv.set('latest', item.ScriptEvaluateId)
+        }
+    }
+
+    return NextResponse.json({});
+}
