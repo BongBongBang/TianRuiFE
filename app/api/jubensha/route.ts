96d5b6c8ceb1a09eb9bdb47af2eb2908a3c6f5e7
PC/Aaron <343129625@qq.com>
2024-01-18 21:20:09 +0800

    123

--- app/api/jubensha/route.ts
+++ app/api/jubensha/route.ts
@@ -12,36 +12,38 @@ export const runtime = 'edge'
 export async function POST(req: Request) {
     const json = await req.json()
     const { idGt, idLt } = json
+    console.log('start request', Date.now());
     const result = (await fetch(`http://go.1jtec.com/fetch_comment?idGt=${idGt}&idLt=${idLt}`, {method: 'GET'}));
     const array = await result.json();
+    console.log('end request', Date.now());
 
-    for (let i = 0; i < array.length; i++) {
+    // for (let i = 0; i < array.length; i++) {
 
-        const item = array[i]
+    //     const item = array[i]
 
-        const messages : any = [{role:'user', content: item.EvaluateTextContent}];
-        const res = await openai.chat.completions.create({
-            model: 'gpt-3.5-turbo',
-            messages,
-            temperature: 0.7,
-            stream: false
-        })
+    //     const messages : any = [{role:'user', content: item.EvaluateTextContent}];
+    //     const res = await openai.chat.completions.create({
+    //         model: 'gpt-3.5-turbo',
+    //         messages,
+    //         temperature: 0.7,
+    //         stream: false
+    //     })
 
-        const convertedContent = res['choices'][0]['message']['content']
-        const body : any = [{
-            "scriptEvaluateId": item.ScriptEvaluateId,
-            "scriptId": item.ScriptId,
-            "evaluateTextContent": convertedContent
-        }]
-        const result = await fetch(`http://go.1jtec.com/save_converts`, {
-            method: 'POST',
-            body: JSON.stringify(body)
-        });
-        if (result.ok) {
-            await kv.set('latest', item.ScriptEvaluateId)
-        }
-        console.log('Success ', item.ScriptEvaluateId); 
-    }
+    //     const convertedContent = res['choices'][0]['message']['content']
+    //     const body : any = [{
+    //         "scriptEvaluateId": item.ScriptEvaluateId,
+    //         "scriptId": item.ScriptId,
+    //         "evaluateTextContent": convertedContent
+    //     }]
+    //     const result = await fetch(`http://go.1jtec.com/save_converts`, {
+    //         method: 'POST',
+    //         body: JSON.stringify(body)
+    //     });
+    //     if (result.ok) {
+    //         await kv.set('latest', item.ScriptEvaluateId)
+    //     }
+    //     console.log('Success ', item.ScriptEvaluateId); 
+    // }
 
     return NextResponse.json({});
 }
