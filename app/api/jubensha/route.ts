05e95e3f8df924ce8ce6017924df45285887fd1f
PC/Aaron <343129625@qq.com>
2024-01-18 21:56:06 +0800

    async

--- app/api/jubensha/route.ts
+++ app/api/jubensha/route.ts
@@ -3,7 +3,7 @@ import OpenAI from 'openai'
 import { kv } from '@vercel/kv'
 
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
+    apiKey: "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"
   })
   
 export const runtime = 'edge'
@@ -12,37 +12,43 @@ export async function POST(req: Request) {
     const json = await req.json()
     const { idGt, idLt } = json
     console.log('start request', Date.now());
-    const result = (await fetch(`http://go.1jtec.com/fetch_comment?idGt=${idGt}&idLt=${idLt}`, {method: 'GET'}));
-    const array = await result.json();
-    console.log('end request', Date.now());
+    fetch(`http://go.1jtec.com/fetch_comment?idGt=${idGt}&idLt=${idLt}`, {method: 'GET'})
+    .then(response => response.json())
+    .then(async array => {
+        console.log('end request', Date.now());
 
-    for (let i = 0; i < array.length; i++) {
-
-        const item = array[i]
-        console.log('start', Date.now(), item.ScriptEvaluateId);
-        const messages : any = [{role:'user', content: item.EvaluateTextContent}];
-        const res = await openai.chat.completions.create({
-            model: 'gpt-3.5-turbo',
-            messages,
-            temperature: 0.7,
-            stream: false
-        })
-        console.log('converted', Date.now(), item.ScriptEvaluateId);
-        const convertedContent = res['choices'][0]['message']['content']
-        const body : any = [{
-            "scriptEvaluateId": item.ScriptEvaluateId,
-            "scriptId": item.ScriptId,
-            "evaluateTextContent": convertedContent
-        }]
-        const result = await fetch(`http://go.1jtec.com/save_converts`, {
-            method: 'POST',
-            body: JSON.stringify(body)
-        });
-        // if (result.ok) {
-        //     await kv.set('latest', item.ScriptEvaluateId)
-        // }
-        console.log('success ', item.ScriptEvaluateId); 
-    }
+        for (let i = 0; i < array.length; i++) {
+    
+            const item = array[i]
+            console.log('converting', Date.now(), item.ScriptEvaluateId);
+            const messages : any = [{role:'user', content: item.EvaluateTextContent}];
+            const res = await openai.chat.completions.create({
+                model: 'gpt-3.5-turbo',
+                messages,
+                temperature: 0.7,
+                stream: false
+            })
+            console.log('converted', Date.now(), item.ScriptEvaluateId);
+            const convertedContent = res['choices'][0]['message']['content']
+            const body : any = [{
+                "scriptEvaluateId": item.ScriptEvaluateId,
+                "scriptId": item.ScriptId,
+                "evaluateTextContent": convertedContent
+            }]
+            const result = fetch(`http://go.1jtec.com/save_converts`, {
+                method: 'POST',
+                body: JSON.stringify(body)
+            });
+            // if (result.ok) {
+            //     await kv.set('latest', item.ScriptEvaluateId)
+            // }
+            console.log('success ', item.ScriptEvaluateId); 
+        }
+    })
+    .catch(error => {
+        console.log(error)
+    })
+    
 
     return NextResponse.json({});
 }
