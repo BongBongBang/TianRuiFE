9d1146c1b2aa9f4e92dc489dcbb712cd20a27937
PC/Aaron <343129625@qq.com>
2024-01-18 21:27:52 +0800

    123321

--- app/api/jubensha/route.ts
+++ app/api/jubensha/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server"
 import OpenAI from 'openai'
 import { kv } from '@vercel/kv'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
@@ -17,33 +16,33 @@ export async function POST(req: Request) {
     const array = await result.json();
     console.log('end request', Date.now());
 
-    // for (let i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length; i++) {
 
-    //     const item = array[i]
-
-    //     const messages : any = [{role:'user', content: item.EvaluateTextContent}];
-    //     const res = await openai.chat.completions.create({
-    //         model: 'gpt-3.5-turbo',
-    //         messages,
-    //         temperature: 0.7,
-    //         stream: false
-    //     })
-
-    //     const convertedContent = res['choices'][0]['message']['content']
-    //     const body : any = [{
-    //         "scriptEvaluateId": item.ScriptEvaluateId,
-    //         "scriptId": item.ScriptId,
-    //         "evaluateTextContent": convertedContent
-    //     }]
-    //     const result = await fetch(`http://go.1jtec.com/save_converts`, {
-    //         method: 'POST',
-    //         body: JSON.stringify(body)
-    //     });
-    //     if (result.ok) {
-    //         await kv.set('latest', item.ScriptEvaluateId)
-    //     }
-    //     console.log('Success ', item.ScriptEvaluateId); 
-    // }
+        const item = array[i]
+        console.log('start', Date.now(), item.ScriptEvaluateId);
+        const messages : any = [{role:'user', content: item.EvaluateTextContent}];
+        const res = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo',
+            messages,
+            temperature: 0.7,
+            stream: false
+        })
+        console.log('converted', Date.now(), item.ScriptEvaluateId);
+        const convertedContent = res['choices'][0]['message']['content']
+        const body : any = [{
+            "scriptEvaluateId": item.ScriptEvaluateId,
+            "scriptId": item.ScriptId,
+            "evaluateTextContent": convertedContent
+        }]
+        const result = await fetch(`http://go.1jtec.com/save_converts`, {
+            method: 'POST',
+            body: JSON.stringify(body)
+        });
+        // if (result.ok) {
+        //     await kv.set('latest', item.ScriptEvaluateId)
+        // }
+        console.log('success ', item.ScriptEvaluateId); 
+    }
 
     return NextResponse.json({});
 }
